700f0c66ee2e49a634f7101d09fc605e7fda7819
Jashema Panapa <jcpanapa@gmail.com>
2020-03-31 13:31:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   string = sentence.split(" ")
   reversed_word = string.each do |word|
-   reversed_word << word.reverse!
+   word.reverse!
   end
   return reversed_word.join(" ")
 end
