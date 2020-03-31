6cafb71ffcd4afeba5718c59f411571195422a71
Jashema Panapa <jcpanapa@gmail.com>
2020-03-31 13:26:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,4 @@
-def reverse_each_word(string)
-original_array = string.split(" ")
-  return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
-  end
-  return_array.join(" ")
-end
+
 def reverse_each_word(sentence)
   reversed_string = sentence.split(" ").collect do |x|
       x.reverse
